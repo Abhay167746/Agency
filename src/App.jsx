3a287d08cf4fdfa338ec60e44d8e1ba11f6f2788
Abhay Tiwari <tiwari.abhay7978@gmail.com>
2025-12-28 12:00:17 +0530

    add something

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar"
-import Hero from "./components/hero"
+import Hero from "./components/Hero"
 import Services from "./components/Services"
 import WhyUs from "./components/WhyUs"
 import CTA from "./components/CTA"
